440a034a34de6f437bb38f3c44401cce5422a45e
Dani <das22001@byui.edu>
2025-07-11 20:24:22 +0200

    commit

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,16 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //this array stores the result of length
+        double[] result = new double[length];
+        //the for loop goes from 0 to length - 1
+        //For each index i, set array[i] = number * (i + 1)
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+
+        return result; // return the erray
     }
 
     /// <summary>
@@ -29,5 +38,12 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        int splitIndex = data.Count - amount;//calculats the starting index of the right-rotated portion.
+        List<int> rotatedPart = data.GetRange(splitIndex, amount);//getRange for the amount 
+        List<int> remainingPart = data.GetRange(0, splitIndex);//getRange for the remainingPart
+
+        data.Clear();// clears the original list
+        data.AddRange(rotatedPart);// add the rightmost part first
+        data.AddRange(remainingPart);// then add the rest
     }
 }
